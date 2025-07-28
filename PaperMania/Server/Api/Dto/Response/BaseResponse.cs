21ef1eadb8c7a243d75d08bb961d01e93fcbda47
Seanyee1227 <b01080080329@gmail.com>
2025-07-28 15:51:27 +0900

    feat: BaseResponse 클래스 생성
    #84

---
+++ PaperMania/Server/Api/Dto/Response/BaseResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Api.Dto.Response;
+
+public class BaseResponse<T>
+{
+    public int ErrorCode { get; set; }
+    public string Message { get; set; } = "성공적으로 처리되었습니다.";
+    public T Data { get; set; }
+}
