61c2cc46800fe2d38acd3313e39fb6fb532dcd81
Seanyee1227 <b01080080329@gmail.com>
2025-07-28 21:02:43 +0900

    feat: Response 래핑 클래스 생성
    #84

--- PaperMania/Server/Api/Dto/Response/BaseResponse.cs
+++ PaperMania/Server/Api/Dto/Response/BaseResponse.cs
@@ -3,6 +3,6 @@
 public class BaseResponse<T>
 {
     public int ErrorCode { get; set; }
-    public string Message { get; set; } = "성공적으로 처리되었습니다.";
+    public string Message { get; set; }
     public T? Data { get; set; }
 }
---
+++ PaperMania/Server/Api/Response.cs
@@ -0,0 +1,16 @@
+﻿using Server.Api.Dto.Response;
+
+namespace Server.Api;
+
+public static class Response
+{
+    public static BaseResponse<T> Ok<T>(T data, string message)
+    {
+        return new() { ErrorCode = 0, Message = message, Data = data };
+    }
+
+    public static BaseResponse<T> Error<T>(int code, string message)
+    {
+        return new() { ErrorCode = code, Message = message, Data = default };
+    }
+}
