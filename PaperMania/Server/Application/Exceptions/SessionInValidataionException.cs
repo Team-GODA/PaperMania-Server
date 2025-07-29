c42a25b9e445bd19849de87e3dbb6919a3b01feb
Seanyee1227 <b01080080329@gmail.com>
2025-07-29 23:22:11 +0900

    fix: 이름 수정
    #84

--- PaperMania/Server/Application/Exceptions/SessionInValidataionException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Server.Application.Exceptions;
-
-public class SessionInValidataionException : ApplicationException
-{
-    public SessionInValidataionException(string message = "유효하지 않는 SID 입니다")
-        : base(message)
-    {
-    }
-}
---
+++ PaperMania/Server/Application/Exceptions/SessionValidationException.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.Application.Exceptions;
+
+public class SessionValidationException : ApplicationException
+{
+    public SessionValidationException(string message = "유효하지 않는 SID 입니다")
+        : base(message)
+    {
+    }
+}
