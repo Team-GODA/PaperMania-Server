8e08408d4af6c6bbef4d834168ad02a91efcf10c
Seanyee1227 <b01080080329@gmail.com>
2025-07-29 22:53:45 +0900

    feat: 세션 유효성 검사 예외 생성
    #84

---
+++ PaperMania/Server/Application/Exceptions/SessionInValidataionException.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.Application.Exceptions;
+
+public class SessionInValidataionException : ApplicationException
+{
+    public SessionInValidataionException(string message = "유효하지 않는 SID 입니다")
+        : base(message)
+    {
+    }
+}
