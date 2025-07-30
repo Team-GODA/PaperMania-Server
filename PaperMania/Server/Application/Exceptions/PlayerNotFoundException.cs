0a550ed9d8c280788f53e739e54954382ac96797
Seanyee1227 <b01080080329@gmail.com>
2025-07-30 21:29:02 +0900

    feat: 플래이어 데이터 예외 생성
    #84

---
+++ PaperMania/Server/Application/Exceptions/PlayerNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.Application.Exceptions;
+
+public class PlayerNotFoundException : Exception
+{
+    public PlayerNotFoundException(int? userId)
+        : base($"{userId} 에 대한 플레이어 데이터를 찾을 수 없습니다.")
+    {
+    }
+}
