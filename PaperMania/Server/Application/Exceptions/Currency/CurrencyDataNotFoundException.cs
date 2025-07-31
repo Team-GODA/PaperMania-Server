c82d9d6404a849cc516ee377c3940ae243f5d672
Seanyee1227 <b01080080329@gmail.com>
2025-08-01 05:06:05 +0900

    feat: 플레이어 재화 데이터 확인 예외 추가
    #84

---
+++ PaperMania/Server/Application/Exceptions/Currency/CurrencyDataNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using Server.Api.Dto.Response;
+
+namespace Server.Application.Exceptions.Currency;
+
+public class CurrencyDataNotFoundException : GameException
+{
+    public CurrencyDataNotFoundException(int? userId)
+        : base(ErrorStatusCode.NotFound, $"Id : {userId}의 재화 데이터가 없습니다.")
+    {
+    }
+}
