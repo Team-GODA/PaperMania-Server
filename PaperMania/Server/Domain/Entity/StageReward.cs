2ca3ee2b74f81618e1ad55226b746d9cd70b4182
Sean-mn <b01080080329@gmail.com>
2025-09-02 10:32:56 +0900

    fix: 변수 이름 수정
    - StageReward 변수 이름 수정 (SubStageNum -> StageSubNum)

--- PaperMania/Server/Domain/Entity/StageReward.cs
+++ PaperMania/Server/Domain/Entity/StageReward.cs
@@ -3,7 +3,7 @@
 public class StageReward
 {
     public int StageNum { get; set; }
-    public int SubStageNum { get; set; }
+    public int StageSubNum { get; set; }
     public int Gold { get; set; }
     public int PaperPiece { get; set; }
     public int ClearExp { get; set; }
--- PaperMania/Server/Infrastructure/Repository/RewardRepository.cs
+++ PaperMania/Server/Infrastructure/Repository/RewardRepository.cs
@@ -17,13 +17,13 @@ public class RewardRepository : RepositoryBase, IRewardRepository
 
         var sql = @"
             SELECT stage_num AS StageNum, 
-               stage_sub_num AS SubStageNum, 
+               stage_sub_num AS StageSubNum, 
                clear_paper_piece AS PaperPiece, 
                clear_gold AS Gold, 
                clear_exp AS ClearExp
         FROM paper_mania_stage_data.stage_reward 
         WHERE stage_num = @StageNum 
-          AND stage_sub_num = @SubStageNum";
+          AND stage_sub_num = @StageSubNum";
 
         var result = await db.QueryFirstOrDefaultAsync<StageReward>(sql, new
         {
