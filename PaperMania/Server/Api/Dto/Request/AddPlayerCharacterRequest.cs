06226a6a05f2b251d68677bb08179a90a3b5deaf
Sean-mn <b01080080329@gmail.com>
2025-09-16 15:05:28 +0900

    fix: 캐릭터 추가 응답/요청 Dto 수정
    #99

--- PaperMania/Server/Api/Dto/Request/AddPlayerCharacterRequest.cs
+++ PaperMania/Server/Api/Dto/Request/AddPlayerCharacterRequest.cs
@@ -1,18 +1,13 @@
-﻿namespace Server.Api.Dto.Request
+﻿using Server.Domain.Entity;
+
+namespace Server.Api.Dto.Request
 {
     /// <summary>
     /// 플레이어가 보유한 캐릭터 추가 요청 DTO
     /// </summary>
     public class AddPlayerCharacterRequest
     {
-        /// <summary>
-        /// 플레이어 고유 ID
-        /// </summary>
         public int Id { get; set; }
-        
-        /// <summary>
-        /// 추가할 캐릭터의 ID
-        /// </summary>
-        public string CharacterId { get; set; } = null!;
+        public CharacterData Data { get; set; } = null!;
     }
 }
