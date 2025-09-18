4208125dec0a99df490b639253e133075eea744f
Sean-mn <b01080080329@gmail.com>
2025-09-19 08:20:08 +0900

    fix: CharacterRepository 수정
    #99

--- PaperMania/Server/Infrastructure/Repository/CharacterRepository.cs
+++ PaperMania/Server/Infrastructure/Repository/CharacterRepository.cs
@@ -16,22 +16,20 @@ public class CharacterRepository : RepositoryBase, ICharacterRepository
         _cache = cache;
     }
 
-    public async Task<IEnumerable<PlayerCharacterData>> GetPlayerCharactersDataByUserIdAsync(int? userId)
+    public async Task<IEnumerable<PlayerCharacterData>> GetPlayerCharactersDataByUserIdAsync(int userId)
     {
-        if (userId == null)
-            throw new RequestException(ErrorStatusCode.NotFound, "USER_NOT_FOUND",
-                new { UserId = userId });
-
         await using var db = CreateConnection();
         await db.OpenAsync();
 
         var sql = @"
-            SELECT user_id AS UserId, character_id AS CharacterId,
-                   character_level AS CharacterLevel,
-                   normal_skill_level AS NormalSkillLevel,
-                   epic_skill_level AS EpicSkillLevel,
-                   character_pieces AS PieceAmount
-            FROM paper_mania_game_data.player_character_data
+            SELECT PC.user_id AS UserId, PC.character_id AS CharacterId,
+                   PC.character_level AS CharacterLevel,
+                   PC.normal_skill_level AS NormalSkillLevel,
+                   PC.epic_skill_level AS EpicSkillLevel,
+                   PP.character_piece AS PieceAmount
+            FROM paper_mania_game_data.player_character_data PC
+            JOIN paper_mania_game_data.player_character_piece_data PP
+                ON PC.user_id = PP.user_id AND PC.character_id = PP.character_id
             WHERE user_id = @UserId
         ";
 
@@ -55,14 +53,26 @@ public class CharacterRepository : RepositoryBase, ICharacterRepository
         await using var db = CreateConnection();
         await db.OpenAsync();
 
-        var sql = @"
+        var insertCharacterSql = @"
             INSERT INTO paper_mania_game_data.player_character_data
-            (user_id, character_id, character_level, normal_skill_level, epic_skill_level, character_pieces)
-            VALUES (@UserId, @CharacterId, 1, 1, 1, 0);
+            (user_id, character_id, character_level, normal_skill_level, epic_skill_level)
+            VALUES (@UserId, @CharacterId, 1, 1, 1);
         ";
 
-        await db.ExecuteAsync(sql, new { UserId = data.UserId, CharacterId = data.Data.CharacterId });
+        var insertPieceSql = @"
+            INSERT INTO paper_mania_game_data.player_character_piece_data
+            (user_id, character_id, character_piece)
+            VALUES (@UserId, @CharacterId, 0);
+        ";
 
+        var param = new { UserId = data.UserId, CharacterId = data.Data.CharacterId };
+        await using var transaction = await db.BeginTransactionAsync();
+        
+        await db.ExecuteAsync(insertCharacterSql, param, transaction);
+        await db.ExecuteAsync(insertPieceSql, param, transaction);
+
+        await transaction.CommitAsync();
+        
         var baseData = _cache.GetCharacter(data.Data.CharacterId);
         if (baseData == null)
             throw new RequestException(ErrorStatusCode.NotFound, "CHARACTER_NOT_FOUND",
@@ -95,10 +105,9 @@ public class CharacterRepository : RepositoryBase, ICharacterRepository
         await db.OpenAsync();
 
         var sql = @"
-            UPDATE paper_mania_game_data.player_character_data
-            SET character_pieces = character_pieces + @Amount
+            UPDATE paper_mania_game_data.player_character_piece_data
+            SET character_piece = character_piece + @Amount
             WHERE user_id = @UserId AND character_id = @CharacterId
-            RETURNING character_pieces;
         ";
 
         await db.ExecuteAsync(sql, new { Amount = amount, UserId = userId, CharacterId = characterId });
