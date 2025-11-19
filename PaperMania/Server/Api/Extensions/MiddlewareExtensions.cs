80a61785386dbcca8c2c97db89c0daab8a341577
Sean-mn <b01080080329@gmail.com>
2025-11-19 14:55:02 +0900

    feat: MiddlewareExtensions.cs 생성
    #114

---
+++ PaperMania/Server/Api/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Server.Api.Extensions;
+
+public static class MiddlewareExtensions
+{
+    public static IApplicationBuilder UseSwaggerConfiguration
+        (this IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v3/swagger.json", "PaperMania API V3");
+        });
+
+        return app;
+    }
+}
