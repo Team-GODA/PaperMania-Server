38ea03e1994a750a7de9fe1f3a1ba61b4a306a25
PARK HYEONMIN (박현민) <b01080080329@gmail.com>
2025-12-01 02:22:57 +0900

    feat: CacheKey 관리 클래스 생성
    #133

---
+++ PaperMania/Server/Infrastructure/Cache/CacheKey.cs
@@ -0,0 +1,39 @@
+﻿namespace Server.Infrastructure.Cache;
+
+public static class CacheKey
+{
+    /// <summary>
+    /// 플레이어 계정 데이터
+    /// Key: PlayerAccountData:{userId}
+    /// </summary>
+    public static string PlayerAccountDataByUserId(int userId)
+        => $"PlayerAccountData:{userId}";
+    
+    /// <summary>
+    /// 플레이어 계정 데이터
+    /// Key: PlayerAccountData:PlayerId:{playerId}
+    /// </summary>
+    public static string PlayerAccountDataByPlayerId(string playerId)
+        => $"PlayerAccountData:PlayerId:{playerId}";
+    
+    /// <summary>
+    /// 플레이어 게임 데이터
+    /// Key: PlayerGameData:{userId}
+    /// </summary>
+    public static string PlayerGameData(int userId)
+        => $"PlayerGameData:{userId}";
+    
+    /// <summary>
+    /// 세션 데이터
+    /// Key: Session:{sessionId}
+    /// </summary>
+    public static string Session(string sessionId)
+        => $"Session:{sessionId}";
+    
+    /// <summary>
+    /// UserId로 SessionId 조회
+    /// Key: Session:UserId:{userId}
+    /// </summary>
+    public static string SessionByUserId(int? userId)
+        => $"Session:UserId:{userId}";
+}
