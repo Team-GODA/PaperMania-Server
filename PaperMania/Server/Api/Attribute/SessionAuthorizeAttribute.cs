5fc87f0efc5103391ff6866dc5056ab2c0e33082
Sean-mn <b01080080329@gmail.com>
2025-12-08 17:23:59 +0900

    feat: Session 인가 어트리뷰트 추가
    #134

---
+++ PaperMania/Server/Api/Attribute/SessionAuthorizeAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Server.Api.Filter;
+
+namespace Server.Api.Attribute;
+
+public class SessionAuthorizeAttribute : TypeFilterAttribute
+{
+    public SessionAuthorizeAttribute() : base(typeof(SessionValidationFilter))
+    {
+        
+    }
+}
