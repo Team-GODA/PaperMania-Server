7d5379338395b9f44f7fb88640078586ae8388d2
PARK HYEONMIN (박현민) <b01080080329@gmail.com>
2026-02-23 02:08:32 +0900

    feat: 로그인시 캐시 어사이드 추가

--- PaperMania/Server/Application/UseCase/Auth/LoginUseCase.cs
+++ PaperMania/Server/Application/UseCase/Auth/LoginUseCase.cs
@@ -6,6 +6,7 @@ using Server.Application.Port.Output.Service;
 using Server.Application.UseCase.Auth.Command;
 using Server.Application.UseCase.Auth.Result;
 using Server.Domain.Service;
+using Server.Infrastructure.Cache;
 
 namespace Server.Application.UseCase.Auth;
 
@@ -14,23 +15,31 @@ public class LoginUseCase : ILoginUseCase
     private readonly IAccountRepository _repository;
     private readonly ISessionService _sessionService;
     private readonly IPasswordHasher _passwordHasher;
+    private readonly CacheAsideService _cacheAsideService;
 
     public LoginUseCase(
         IAccountRepository repository,
         ISessionService sessionService,
-        IPasswordHasher passwordHasher
+        IPasswordHasher passwordHasher,
+        CacheAsideService cacheAsideService
     )
     {
         _repository = repository;
         _sessionService = sessionService;
         _passwordHasher = passwordHasher;
+        _cacheAsideService = cacheAsideService;
     }
     
     public async Task<LoginResult> ExecuteAsync(LoginCommand request)
     {
         request.Validate();
         
-        var account = await _repository.FindByPlayerIdAsync(request.PlayerId);
+        var account = await _cacheAsideService.GetOrSetAsync(
+            CacheKey.Account.ByPlayerId(request.PlayerId),
+            async () => await _repository.FindByPlayerIdAsync(request.PlayerId),
+            expiration: TimeSpan.FromDays(7)
+        );
+        
         if (account == null || string.IsNullOrEmpty(account.Password))
         {
             _passwordHasher.Verify(request.Password, "DUMMY_HASH");
