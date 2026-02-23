bbc886be3e2ca53069b64f5bb46c496502c96128
PARK HYEONMIN (박현민) <b01080080329@gmail.com>
2026-02-24 03:20:08 +0900

    modify: RequestException 수정

--- PaperMania/Server/Application/Exceptions/RequestException.cs
+++ PaperMania/Server/Application/Exceptions/RequestException.cs
@@ -5,6 +5,7 @@ namespace Server.Application.Exceptions;
 public class RequestException : GameException
 {
     public object?[] Context { get; }
+
     public RequestException(ErrorStatusCode statusCode, string message, params object?[] context)
         : base(statusCode, message)
     {
